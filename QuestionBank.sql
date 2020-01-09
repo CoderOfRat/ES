a16fbd938dcd481865dfbba062e07afe25a53c97
Wenhao Guo <1404616446@qq.com>
2020-01-09 09:07:38 +0800

    basefiles
    design dependencies

---
+++ QuestionBank.sql
@@ -0,0 +1,65 @@
+CREATE TABLE c_bank_judge (
+    number INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title text,
+    solution text
+)default charset utf8;
+
+create table c_bank_single (
+	number INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title text,
+	A text,
+    B text,
+    C text,
+    D text,
+    solution text
+)default charset utf8;
+
+CREATE TABLE c_bank_JQquiz (
+    number INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title text,
+    solution text
+)default charset utf8;
+
+CREATE TABLE cpp_bank_judge (
+    number INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title text,
+    solution text
+)default charset utf8;
+
+create table cpp_bank_single (
+	number INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title text,
+	A text,
+    B text,
+    C text,
+    D text,
+    solution text
+)default charset utf8;
+
+CREATE TABLE cpp_bank_JQquiz (
+    number INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title text,
+    solution text
+)default charset utf8;
+
+CREATE TABLE java_bank_judge (
+    number INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title text,
+    solution text
+)default charset utf8;
+
+create table java_bank_single (
+	number INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title text,
+	A text,
+    B text,
+    C text,
+    D text,
+    solution text
+)default charset utf8;
+
+CREATE TABLE java_bank_JQquiz (
+    number INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title text,
+    solution text
+)default charset utf8;
---
+++ users.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users (
+    id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(20),
+    password VARCHAR(16)
+)default charset=utf8;
